108d68802e87504ceb56212ad603f84544b1b122
Endrit Kane <endritkane95@gmail.com>
2018-08-09 23:25:40 +0200

    added base files

---
+++ carousel.php
@@ -0,0 +1,120 @@
+    <!-- Header Carousel -->
+    <header id="cover" class="carousel slide cover">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+			
+            	<li data-target="#cover" data-slide-to="0" class="active"></li>
+          		
+            	<li data-target="#cover" data-slide-to="1" ></li>
+          		
+            	<li data-target="#cover" data-slide-to="2" ></li>
+          		
+            	<li data-target="#cover" data-slide-to="3" ></li>
+          		
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+			
+	            <div class="item active">
+	            	<a title="Vuoi Bronx in Blu-Ray?" href="film-dvd/bronx/f3614/index.html">
+	            		
+		                <div class="fill" title="Vuoi Bronx in Blu-Ray?" style="background-image:url('../cloud.cgentertainment.it/img/homepage/BRONX_HEAD.jpg');"></div>
+		            </a>
+		            	
+						    	<div class="carousel-categoria carousel-lineheight" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">NEWS</div>
+							
+						<a title="Vuoi Bronx in Blu-Ray?" href="film-dvd/bronx/f3614/index.html">
+			                <div class="carousel-titolo" style="color:#ffffff; text-shadow: 1px 1px #191919;">
+								Vuoi Bronx in Blu-Ray?
+							    	<div class="carousel-abstract carousel-lineheight" style="color:#ffffff;">Un film di Robert De Niro </div>
+								
+							    	<div class="carousel-regia carousel-lineheight hidden-xs" style="color:#ffffff;">Partita la Start Up per realizzare Bronx in Blu-Ray Limited Edition!<br /><em>Pre-acquistalo a soli €14,99 spese di spedizione incluse! </em></div>
+								
+									<div style="clear:both; height:20px;"></div>
+									
+							    		<div class="carousel-calltoaction" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">SCOPRI</div>
+								
+				            </div>
+			            </a>
+	            </div>
+				
+	            <div class="item">
+	            	<a title="The Wicked Gift" href="news/thewickedgiftdirobertodantonaindvdebluraydaagosto/index.html">
+	            		
+		                <div class="fill" title="The Wicked Gift" style="background-image:url('../cloud.cgentertainment.it/img/homepage/THE-WICKED-GIFT_HEAD.jpg');"></div>
+		            </a>
+		            	
+						    	<div class="carousel-categoria carousel-lineheight" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">SCOPRI </div>
+							
+						<a title="The Wicked Gift" href="news/thewickedgiftdirobertodantonaindvdebluraydaagosto/index.html">
+			                <div class="carousel-titolo" style="color:#ffffff; text-shadow: 1px 1px #191919;">
+								The Wicked Gift 
+							    	<div class="carousel-abstract carousel-lineheight" style="color:#ffffff;">Un film di e con Roberto d'Antona </div>
+								
+							    	<div class="carousel-regia carousel-lineheight hidden-xs" style="color:#ffffff;">Quale segreto si nasconde nei tuoi incubi?<br /><em>In DVD e Blu-Ray CG Entertainment dal 30 Agosto</em></div>
+								
+									<div style="clear:both; height:20px;"></div>
+									
+							    		<div class="carousel-calltoaction" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">SCOPRI</div>
+								
+				            </div>
+			            </a>
+	            </div>
+				
+	            <div class="item">
+	            	<a title="Donnie Brasco in Blu-Ray!" href="film-dvd/donnie-brasco/f0935/index.html">
+	            		
+		                <div class="fill" title="Donnie Brasco in Blu-Ray!" style="background-image:url('../cloud.cgentertainment.it/img/homepage/0935.jpg');"></div>
+		            </a>
+		            	
+						    	<div class="carousel-categoria carousel-lineheight" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">START UP! </div>
+							
+						<a title="Donnie Brasco in Blu-Ray!" href="film-dvd/donnie-brasco/f0935/index.html">
+			                <div class="carousel-titolo" style="color:#ffffff; text-shadow: 1px 1px #191919;">
+								Donnie Brasco in Blu-Ray!
+							    	<div class="carousel-abstract carousel-lineheight" style="color:#ffffff;">Un film di Mike Newell</div>
+								
+							    	<div class="carousel-regia carousel-lineheight hidden-xs" style="color:#ffffff;">Pre-acquista la tua copia in Edizione Limitata - 2 Blu-Ray a soli €17,99! <br /><em>Poche copie rimaste! </em></div>
+								
+									<div style="clear:both; height:20px;"></div>
+									
+							    		<div class="carousel-calltoaction" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">SCOPRI</div>
+								
+				            </div>
+			            </a>
+	            </div>
+				
+	            <div class="item">
+	            	<a title="La carriera di Carlo Vanzina in 5 film" href="news/lacarrieradicarlovanzinain5film/index.html">
+	            		
+		                <div class="fill" title="La carriera di Carlo Vanzina in 5 film" style="background-image:url('../cloud.cgentertainment.it/img/homepage/VANZINA_HEAD.jpg');"></div>
+		            </a>
+		            	
+						    	<div class="carousel-categoria carousel-lineheight" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">NEWS</div>
+							
+						<a title="La carriera di Carlo Vanzina in 5 film" href="news/lacarrieradicarlovanzinain5film/index.html">
+			                <div class="carousel-titolo" style="color:#ffffff; text-shadow: 1px 1px #191919;">
+								La carriera di Carlo Vanzina in 5 film 
+							    	<div class="carousel-abstract carousel-lineheight" style="color:#ffffff;">Ci lascia a 67 anni</div>
+								
+							    	<div class="carousel-regia carousel-lineheight hidden-xs" style="color:#ffffff;">Figlio di Steno, insieme al fratello Enrico ha raccontato tic e vizi dell'Italia tra gli '80 ed i '90 <br /><em>Lo ricordiamo con 5 film significativi per la sua carriera </em></div>
+								
+									<div style="clear:both; height:20px;"></div>
+									
+							    		<div class="carousel-calltoaction" style="text-shadow: 0px 0px #191919; background-color:#dc0c1d; color:#ffffff;">SCOPRI</div>
+								
+				            </div>
+			            </a>
+	            </div>
+				
+        </div>
+
+        <!-- Controls -->
+        <a title="Sinistra" class="left carousel-control" href="#cover" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a title="Destra" class="right carousel-control" href="#cover" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
---
+++ header.php
@@ -0,0 +1,108 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="HTTrack is an easy-to-use website mirror utility. It allows you to download a World Wide website from the Internet to a local directory,building recursively all structures, getting html, images, and other files from the server to your computer. Links are rebuiltrelatively so that you can freely browse to the local site (works with any browser). You can mirror several sites together so that you can jump from one toanother. You can, also, update an existing mirror site, or resume an interrupted download. The robot is fully configurable, with an integrated help" />
+	<meta name="keywords" content="httrack, HTTRACK, HTTrack, winhttrack, WINHTTRACK, WinHTTrack, offline browser, web mirror utility, aspirateur web, surf offline, web capture, www mirror utility, browse offline, local  site builder, website mirroring, aspirateur www, internet grabber, capture de site web, internet tool, hors connexion, unix, dos, windows 95, windows 98, solaris, ibm580, AIX 4.0, HTS, HTGet, web aspirator, web aspirateur, libre, GPL, GNU, free software" />
+	<title>Local index </title>
+  <link type="text/css" href="assets/css/bootstrap.css" rel="stylesheet"></link>
+
+  <link type="text/css" href="assets/css/bootstrap77ae.css" rel="stylesheet"></link>
+  <link type="text/css" href="assets/css/bootstrap053b.css" rel="stylesheet"></link>
+
+  <link type="text/css" href="assets/css/modern-business99fa.css" rel="stylesheet"></link>
+  <link type="text/css" href="assets/css/modern-business7346.css" rel="stylesheet"></link>
+  
+  <link type="text/css" href="assets/css/custom.css" rel="stylesheet"></link>
+
+</head>
+
+<nav class="navbar navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a title="Home" href="../../index.html"><img class="navbar-brand" src="assets/img/logo-menu-01.png" title="CG ENTERTAINMENT" alt="CG ENTERTAINMENT" /></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="max-height: none;">
+            <div class="hidden-xs nav navbar-nav navbar-right" style="float: right; height:50px; background-color:#ffffff; text-align:right; padding-top: 10px; margin-right: 0px;">
+                <form method="get" action="https://www.cgentertainment.it/esplora/">
+                    <input type="text" class="input_cerca" name="cerca" onchange="$('.input_cerca').val($(this).val());" placeholder="Cerca ..." style="font-size:13px; font-family:Helvetica, Arial, sans-serif; font-weight: 100; color:#2a363b; padding:5px;">
+                    <input type="submit" value="cerca" style="margin-left:5px; font-weight: 100; font-size:13px; font-family:Helvetica, Arial, sans-serif; border:0px; background-color:#dc0c1d; color:#ffffff; padding:7px; padding-right:15px; padding-left:15px;">
+                </form>
+
+            </div>
+            <ul class="nav navbar-nav navbar-right" style="margin-top: -17px;">
+                <li class="visible-xs" style="margin-top: 17px;">
+                    <div style="background-color:#ffffff; text-align:right; margin-right:-5px;">
+                        <form method="get" action="https://www.cgentertainment.it/esplora/">
+                            <input type="text" class="input_cerca" name="cerca" onchange="$('.input_cerca').val($(this).val());" placeholder="Cerca ..." style="font-size:13px; font-family:Helvetica, Arial, sans-serif; font-weight: 100; color:#2a363b; padding:5px;">
+                            <input type="submit" value="cerca" style="margin-left:5px; font-weight: 100; font-size:13px; font-family:Helvetica, Arial, sans-serif; border:0px; background-color:#dc0c1d; color:#ffffff; padding:7px; padding-right:15px; padding-left:15px;">
+                        </form>
+
+                    </div>
+                    <div style="clear:both;"></div>
+                </li>
+                <li><a title="Home" href="../../index.html">HOME</a></li>
+
+                    <li><a title="Start Up!" href="../../startup/index.html">START UP!</a></li>
+
+                    <li><a title="Start Up!" href="../../esclusive/index.html">ESCLUSIVE</a></li>
+
+                    <li><a title="Promozioni" href="../../promozioni/index.html">PROMOZIONI</a></li>
+
+                    <li><a title="Novità" href="../../novita/index.html">NOVIT&Agrave;</a></li>
+
+                    <li><a title="Prossimamente" href="../../prossimamente/index.html">PROSSIMAMENTE</a></li>
+
+                    <li><a title="News" href="../../extra/index.html">NEWS</a></li>
+
+
+                <li><a title="Area Rivenditori" target="_blank" href="https://sell.cgent.it/">AREA RIVENDITORI</a></li>
+
+
+
+                    <li><a title="Video" href="../../video/index.html">VIDEO</a></li>
+
+
+                <li class="dropdown">
+                    <a title="Canali" href="#" class="dropdown-toggle" data-toggle="dropdown">CANALI <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+
+                            <li><a title="COMEDY" href="../comedy/index.html">COMEDY</a></li>
+
+                            <li><a title="ACTION" href="index.html">ACTION</a></li>
+
+                            <li><a title="INDIE" href="../indie/index.html">INDIE</a></li>
+
+                            <li><a title="HORROR" href="../horror/index.html">HORROR</a></li>
+
+                            <li><a title="CLASSIC" href="../classic/index.html">CLASSIC</a></li>
+
+                            <li><a title="DOC" href="../docu/index.html">DOC</a></li>
+
+                            <li><a title="CULT" href="../cult/index.html">CULT</a></li>
+
+                            <li><a title="ANIMATION" href="../animation/index.html">ANIMATION</a></li>
+
+                    </ul>
+                </li>
+
+                <li><a title="Esplora" href="../../esplora/index.html">ESPLORA</a></li>
+
+
+                <li id="menu_utente"></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+
+
+    </div>
+    <!-- /.container -->
+</nav>
